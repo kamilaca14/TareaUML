9b3baf978fa1a2b8128cec6920162d8fa60dd50c
kamilaca14 <118561867+kamilaca14@users.noreply.github.com>
2022-11-19 12:12:41 -0500

    Add files via upload

---
+++ progra/lector/Pedido.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lector
+{
+    public class Pedido
+    {
+        public string id_del_usuario
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string titulo_del_material
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public del codigo_del_material
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void buscar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void agendar()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void disposicion()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
